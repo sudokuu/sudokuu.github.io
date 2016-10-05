cc5a3b58e1fc175045cfff71c684e5f65ddbf2da
Hugarty Pedro Cavalcante Araújo <RA21450285@uniceub.edu.br>
2016-10-05 10:45:04 -0300

    Adicionando prototipo

---
+++ javinha.js
@@ -0,0 +1,16 @@
+function myFunction() {
+
+  document.getElementById("global").style.display = "none";
+  document.getElementById("menuinf").style.display = "block";
+}
+
+function novo() {
+  document.getElementById("global").style.display = "block";
+  document.getElementById("menuinf").style.display = "none";
+}
+
+function mNovo() {
+  document.getElementById("global").style.display = "block";
+  document.getElementById("menuinf").style.display = "none";
+  
+}
