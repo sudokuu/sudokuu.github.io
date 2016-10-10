d53fff4f50c8ea30cb180f81816e8e8bc4d53142
Felipe Silva <felipehendrix@programmer.net>
2016-10-10 19:47:11 -0300

    Painel de seleção dos números

--- javinha.js
+++ javinha.js
@@ -1,16 +1,68 @@
-function myFunction() {
 
-  document.getElementById("global").style.display = "none";
-  document.getElementById("menuinf").style.display = "block";
+// inicia um novo jogo com a dificuldade indicada
+// 0 - facil
+// 1 - medio
+// 2 - dificil
+function novoJogo(dificuldade) {
+
+  document.getElementById("menu-dificuldade").style.display = "none";
+  document.getElementById("jogo").style.display = "block";
+
+  // pega a tabela do jogo e coloca os eventos necessários e LIMPA todas as celulas
+  for (var i = 0; i < 9; i++) {
+      for(var j = 0; j < 9; j++) {
+          setEventsForCellNamed("a"+ i + j);
+          setCellText("a"+ i + j, "");
+      }
+  }
 }
 
-function novo() {
-  document.getElementById("global").style.display = "block";
-  document.getElementById("menuinf").style.display = "none";
+// exibição da tela inicial
+function telaInicial() {
+  document.getElementById("menu-dificuldade").style.display = "block";
+  document.getElementById("jogo").style.display = "none";
 }
 
-function mNovo() {
-  document.getElementById("global").style.display = "block";
-  document.getElementById("menuinf").style.display = "none";
-  
+// onclick geral
+window.onclick = function(event) {
+    var chooser = document.getElementById("chooser-modal");
+    if (event.target == chooser) {
+        chooser.style.display = "none";
+    }
+}
+
+
+// seta o texto de uma celula
+function setCellText(cellName, text) {
+    var cell = document.getElementById(cellName);
+    cell.innerHTML = text;
+}
+
+// id da celula selecionada para escolher o numero
+var selectedCell = "";
+
+// evento onclick das celulas do jogo
+function cellClick(p) {
+    selectedCell = p.id;
+
+    var chooser = document.getElementById("chooser-modal");
+    chooser.style.display = "block";
+
+    var content = chooser.children[0];
+    var offsets = p.getBoundingClientRect();
+    content.style.top = offsets.top + 35 + "px";
+}
+
+// escolha de um numero no painel
+function chooseNumber(number) {
+    setCellText(selectedCell, number);
+
+    var chooser = document.getElementById("chooser-modal");
+    chooser.style.display = "none";
+}
+
+// seta os eventos necessários da celula com o nome passado
+function setEventsForCellNamed(name) {
+    var cell = document.getElementById(name);
+    cell.onclick = function(){cellClick(cell)};
 }
